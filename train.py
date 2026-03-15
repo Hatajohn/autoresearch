363a859510e50f03c70b6acb36a651296d69fe30
John Hata <hatajohn@gmail.com>
2026-03-15 11:34:48 -0400

    exp2: DEPTH 8->6 (smaller model, more steps per 5min)

--- train.py
+++ train.py
@@ -439,7 +439,7 @@ WARMDOWN_RATIO = 0.5    # fraction of time budget for LR warmdown
 FINAL_LR_FRAC = 0.0     # final LR as fraction of initial
 
 # Model size
-DEPTH = 8               # number of transformer layers
+DEPTH = 6               # number of transformer layers
 DEVICE_BATCH_SIZE = 4    # per-device batch size (reduce if OOM) originally 128
 
 # ---------------------------------------------------------------------------
